e704f34e7342c143da98b3ecc837001188306a02
Basma Bakirci <144848782+besmabakirci1@users.noreply.github.com>
2025-06-11 18:14:48 +0300

    :)

--- 1.Hafta/studentSolutions/besmaBakirci/Question_5.c
+++ 1.Hafta/studentSolutions/besmaBakirci/Question_5.c
@@ -1,7 +1,26 @@
 // Verilen bir dizideki çift sayıların ortalamasını hesaplayın.
+///bence verilen bir array olacak onun her elemanını gezip içindeki elemanların % ( modu ) == 0 ise çift değilse tek demesi ve çift olarak saptadığı elemanları bir array oluşturup onlara atayıp sonra elemanları toplayıp adete bölmeli sonrasında return 0 diyip kodu durdurması lazım. 
+
 #include <stdio.h> 
 
 int main(){
+    int size = 5 ; 
+    int even_numbers[size];
+    int sum , even_count = 0 ; 
+    double average;
     int arr [] = { 3, 4, 5, 6, 7}; 
-    if ()
+    for (int i = 0 ; i < size ; i++){
+        if (arr[i] % 2 == 0){
+            even_numbers[even_count]= arr[i];
+            sum += arr[i];
+            even_count++;
+        }
+    }
+
+    if (even_count > 0){
+        average = (double) sum / even_count;
+        printf("%.2f\n", average); 
+    }
+
+    return 0; 
 }
--- 1.Hafta/studentSolutions/besmaBakirci/Question_6.c
+++ 1.Hafta/studentSolutions/besmaBakirci/Question_6.c
@@ -1,1 +1,12 @@
 // Bir dizi verildiğinde, dizideki tüm elemanları ters çevirin (reverse).
+/// çözüm yöntemim : kullanıcıdan girdi alıp arrayin içine yazılan ve  ne kadar size var onu saptayıp sonrasında o size kadar for döngüsü olacak ve bu for döngüsü first in last out şeklinde yazılacak yani arrayin 0. indeksi size 5 ise 4. indekse sahip olacak 
+
+#include < stdio.h> 
+
+int main(){
+    int size ;
+    printf("Please enter the size of array : ");
+    scanf("%d", &size);
+    int arr [size] ;
+
+}
